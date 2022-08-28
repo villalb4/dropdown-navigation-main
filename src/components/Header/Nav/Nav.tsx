7370a5c72bcac1e140ee2b466b17d525cdd019d3
villalb4 <villalb4angel@gmail.com>
2022-08-28 20:48:57 -0300

    Add(Header, Nav) styles and dropdown menu

--- src/components/Header/Nav/Nav.tsx
+++ src/components/Header/Nav/Nav.tsx
@@ -1,21 +1,72 @@
-import React from 'react'
-import arrow_down from '../../../assets/icon-arrow-down.svg'
+import React, {useState} from 'react'
 import './Nav.css'
+// --- images ---
+import arrow_down from '../../../assets/icon-arrow-down.svg'
+import todo from '../../../assets/icon-todo.svg'
+import calendar from '../../../assets/icon-calendar.svg'
+import reminders from '../../../assets/icon-reminders.svg'
+import planning from '../../../assets/icon-planning.svg'
+
 
 function Nav() {
+
+  const [hover, setHover] = useState(false)
+
+  function handleHover() {
+    setHover(true)
+  }
+
+  function handleHoverOut() {
+    setHover(false)
+  }
+
   return (
     <nav>
       <ul className='Nav_ul'>
-        <li>
+        <li 
+          onMouseEnter={handleHover}
+          onMouseLeave={handleHoverOut}
+          className='Nav_li'
+        >
           <div className='Nav_lidivContent'>
             <span className='Nav_span'>Features</span>
 						<div className='Nav_divIconArrow'>
             	<img src={arrow_down} alt="arrow" />
 						</div>
           </div>
+          { hover &&
+            <div className='Nav_dropdown'>
+              <ul className='Nav_ulDropdown'>
+                <li className='Nav_liDropdown'>
+                  <div className='Nav_divImageDropdown'>
+                    <img src={todo} alt="Icon Dropdown" />
+                  </div>
+                  <span className='Nav_spanDropdown'>Todo List</span>
+                </li>
+                <li className='Nav_liDropdown'>
+                  <div className='Nav_divImageDropdown'>
+                    <img src={calendar} alt="Icon Dropdown" />
+                  </div>
+                  <span className='Nav_spanDropdown'>Calendar</span>
+                </li>
+                <li className='Nav_liDropdown'>
+                  <div className='Nav_divImageDropdown'>
+                    <img src={reminders} alt="Icon Dropdown" />
+                  </div>
+                  <span className='Nav_spanDropdown'>Reminders</span>
+                </li>
+                <li className='Nav_liDropdown'>
+                  <div className='Nav_divImageDropdown'>
+                    <img src={planning} alt="Icon Dropdown" />
+                  </div>
+                  <span className='Nav_spanDropdown'>Planning</span>
+                </li>
+              </ul>
+            </div>
+          }
         </li>
 
-				<li>
+				<li className='Nav_li'>
           <div className='Nav_lidivContent'>
             <span className='Nav_span'>Company</span>
             <div className='Nav_divIconArrow'>
@@ -24,11 +75,11 @@ function Nav() {
           </div>
         </li>
 
-				<li>
+				<li className='Nav_li'>
           <span className='Nav_span'>Careers</span>
         </li>
 
-				<li>
+				<li className='Nav_li'>
           <span className='Nav_span'>About</span>
         </li>
       </ul>
